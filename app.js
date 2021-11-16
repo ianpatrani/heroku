71e5a288e6a04741a83b6817a2161d67df63b83e
ianpatrani <ianpatrani@hotmail.com>
2021-11-16 17:00:57 -0300

    charge new form html

--- app.js
+++ app.js
@@ -14,13 +14,15 @@ let users = ["bart", "lisa", "homero", "marge"];
 
 app.use(cors());
 app.use(methodOverride());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/users", (req, res) => {
     res.send(users);
 });
 
-app.post("/user/create/:nombre", (req, res) => {
-    users.push(req.params.nombre);
+app.post("/user/create", (req, res) => {
+    users.push(req.body.nombre);
     res.send("usuario creado");
 });
 
