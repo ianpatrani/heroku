e532f8e53d198f1fbc56ea0fbbb0a76900a83d01
ianpatrani <ianpatrani@hotmail.com>
2021-11-16 16:34:47 -0300

    .

---
+++ app.js
@@ -0,0 +1,40 @@
+const methodOverride = require("method-override");
+const cors = require("cors");
+const express = require("express");
+/* import methodOverride from "method-override";
+import cors from "cors";
+import express from "express"; */
+//
+const app = express();
+const log = console.log;
+
+let port = process.env.PORT || 3000;
+
+let users = ["bart", "lisa", "homero", "marge"];
+
+app.use(cors());
+app.use(methodOverride());
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+
+app.post("/user/create/:nombre", (req, res) => {
+    users.push(req.params.nombre);
+    res.send("usuario creado");
+});
+
+app.delete("/user/delete/:nombre", (req, res) => {
+    users = users.filter(user => user !== req.params.nombre);
+    res.send(`usuario eliminado`);
+});
+
+app.put("/users/:nombreAnterior/:nombreNuevo", (req, res) => {
+    users = users.filter(user => user !== req.params.nombreAnterior);
+    users.push(req.params.nombreNuevo);
+    res.send(`usuario cambiado`);
+});
+
+app.listen(port, () => {
+    log("start server");
+});
